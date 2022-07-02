3c6189c72a94e196196627cca4da279a5748e968
nikhileshbaghel <88043998+nikhileshbaghel@users.noreply.github.com>
2022-07-02 18:12:18 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <div className="collapse navbar-collapse" id="navbarCollapse">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
           <Link className= {`nav-link ${(location.pathname==='/home')?"active":""}`} aria-current="page" to="/home">Home</Link>
