8442dd0a17feefdc4e9ec87c04b40838085631b6
nikhileshbaghel <88043998+nikhileshbaghel@users.noreply.github.com>
2023-04-26 15:56:08 +0530

    Update UserWithLogin.js

--- src/components/UserWithLogin.js
+++ src/components/UserWithLogin.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 const UserWithLogin=()=> {
     const Userinfo = async () => {
-        const response = await fetch("https://3.112.37.186/api/auth/getuser", {   //backend url changed  // from https://inotebook-backend-17.herokuapp.com/ to 
+        const response = await fetch("http://13.113.192.171/api/auth/getuser", {   //backend url changed  // from https://inotebook-backend-17.herokuapp.com/ to 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
